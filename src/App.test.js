e765df1c2d40b0d54b932e9b24d19ad7cbbc2fb9
eperez045 <eperez045@gmail.com>
2022-09-13 15:11:11 +0200

    testing, userEvent.click

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+
 
 test('renders search bar', () => {
   render(<App />);
@@ -11,4 +13,19 @@ test('renders buscar text', () => {
   render(<App />);
   const linkElement = screen.getByText(/buscar/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('should show text content as gaztea', () => {
+  render(<App />);
+  const input = screen.getByRole('input');
+  const button = screen.getByRole('button');
+  const textbox = screen.getAllByRole('p')
+  
+  // Simulate typing 'gaztea'
+  userEvent.type(input, '');
+  // Simulate clicking button
+  userEvent.click(button);
+  
+  // Assert textbox has text content 'Hello!'
+  expect(textbox).toHaveValue('');
 });
